54d71f378e13e3cfc27df32e1471fee6c15ecafb
Jackie <giacatkhongminh93@gmail.com>
2021-07-25 02:56:20 +0700

    Binding Event Handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,14 @@
+  
 import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
         count: 0,
+    }
+
+    constructor(){
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this);
     }
 
     styles ={
@@ -11,11 +17,22 @@ class Counter extends Component {
         color: "red"
     }
 
+    handleIncrement = () => {
+        console.log("Jackie is here", this);
+
+    /*
+    //----- Other way not using constructor
+
+    handleIncrement = () => {
+        console.log("Jackie is here", this);
+    }
+    */
+
     render() { 
         return (
             <div>
                 <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
         
