aacf659a4ba6ec5d1d166e169d27638e096e8d6f
Jackie <giacatkhongminh93@gmail.com>
2021-07-25 02:14:51 +0700

    Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,11 +11,15 @@ class Counter extends Component {
         color: "red"
     }
 
+    handleIncrement(){
+        console.log("Jackie is here");
+    }
+
     render() { 
         return (
             <div>
                 <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
         
