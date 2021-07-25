636b1d583e84f50a25c0c21aedadae38421441fd
Jackie <giacatkhongminh93@gmail.com>
2021-07-25 17:58:20 +0700

    Passing data to children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,29 +1,45 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+
     state = { 
-        count: 0,
+        value: 0,
+        //TO DO
+        //It will make the mistake hare
+        //Curently, I do not detect the fault
+        // Maybe I will come back later
     }
+
+    constructor(){
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this);
+    };
 
     styles ={
         fontSize: 20,
         fondWeight: "bold",
         color: "red"
-    }
+    };
+
+    handleIncrement() {
+        this.setState({ value: this.state.value + 1});
+    };
 
     render() { 
+        console.log("props",this.props)
+
         return (
             <div>
+                {this.props.children}
                 <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
         
     }
-
     formatCount(){
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
+        const {value} = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 }
  
