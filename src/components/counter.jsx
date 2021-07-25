0859c21545dbbc25840adaf7ba82a7972a3de684
Jackie <giacatkhongminh93@gmail.com>
2021-07-25 12:56:02 +0700

    Passing Event Args

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,28 +8,46 @@ class Counter extends Component {
     constructor(){
         super();
         this.handleIncrement = this.handleIncrement.bind(this);
-    }
+    };
 
     styles ={
         fontSize: 20,
         fondWeight: "bold",
         color: "red"
-    }
+    };
 
-    handleIncrement() {
+    handleIncrement(id) {
+        console.log(id);
         this.setState({ count: this.state.count + 1});
+    };
+
+    /*
+    *throught another helper func like my event handler
+    doHandleIncrement = () => {
+        this.handleIncrement({id : 2});
     }
 
     render() { 
         return (
             <div>
                 <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
         
     }
+    */
 
+    // Inline 
+    render() { 
+        return (
+            <div>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button onClick={() => this.handleIncrement({id: 1}) } className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
+        
+    }
     formatCount(){
         const {count} = this.state;
         return count === 0 ? 'Zero' : count;
